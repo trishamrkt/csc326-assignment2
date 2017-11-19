b2fb456a91f0bddddee3c6f580d0eab4cf148812
Patricia <patsmrkt@gmail.com>
2017-11-19 08:50:27 -0500

    fix for assignment requirements

--- q2.py
+++ q2.py
@@ -11,12 +11,18 @@ def find_product(L):
     lists = map(lambda (i, x): L[i: i+5], enumerate(L))[:num_iterations+1]
     
     # Finds the product of each list in lists
-    products = map(lambda arr: reduce(lambda x,y: x*y, arr), lists)
+    products = map(lambda  arr: reduce(lambda x,y: x*y, arr), lists)
     
     # Sorts products in descending order and returns max 
-    products = sorted(products, reverse=True)
-    return products[0];
+    sorted_products = sorted(products, reverse=True)
+    
+    # Get greatest value and its index
+    mx = sorted_products[0]
+    index = products.index(mx)
+    
+    return (index, mx);
 
 
 if __name__ == "__main__":
+    # Expected output: (4, 15120)
     print find_product([1,2,3,4,5,6,7,8,9])
