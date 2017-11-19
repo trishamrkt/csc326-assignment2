c5b8f186529ab7737bd6bbd5b656c8aef5371fbc
Patricia <patsmrkt@gmail.com>
2017-11-18 21:03:26 -0500

    Complete implementation of find_product function

---
+++ q2.py
@@ -0,0 +1,16 @@
+"""
+    This function finds the maximum product 
+    of 5 consecutive integers in the input 
+    list 'l' using FUNCTIONAL PROGRAMMING
+    (ie no if/else statements, or while loops)
+"""
+def find_product(L):
+    num_iterations = len(L) - 5
+    lists = map(lambda (i, x): L[i: i+5], enumerate(L))[:num_iterations+1]
+    products = map(lambda arr: reduce(lambda x,y: x*y, arr), lists)
+    products = sorted(products, reverse=True)
+    return products[0];
+
+
+if __name__ == "__main__":
+    print find_product([1,2,3,4,5,6,7,8,9])
