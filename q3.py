c9b683e4e4baafa2a7e788a0aaf043859d4756e0
Patricia <patsmrkt@gmail.com>
2017-11-18 19:40:47 -0500

    Complete implementation of reduce function

--- q3.py
+++ q3.py
@@ -9,11 +9,22 @@ def my_map(func, iterable):
         
     return result_map 
 
-def my_reduce(func, iterable, initializer=None):
-    return 
+def my_reduce(func, iterable):
+    if len(iterable) == 1:
+        return iterable[0]
+    
+    else: 
+        prev = iterable[0]
+        for elem in iterable:
+            if iterable.index(elem) != 0:
+                new_val = func(prev, elem)
+                prev = new_val
+        
+    return new_val;
 
 def my_filter():
     return 
 
 if __name__ == "__main__":
     print my_map(lambda x: x**2, range(10))
+    print my_reduce(lambda x,y: x*y, [1,2,3,4])
